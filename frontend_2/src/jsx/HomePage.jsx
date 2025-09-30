0c70f5c10ac3be9d906d150fa0e7f5e72809e896
Alexandros Lappas <alexandros@alexandross-MacBookAir.local>
2025-09-30 10:36:32 +0200

    Compiling project with Angular

--- frontend_2/src/jsx/HomePage.jsx
+++ frontend_2/src/jsx/HomePage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import AuthForm from './AuthForm';
-import SplashText from './SplashText';
 
 export default function LoginPage() {
   return (
     <div className="login-page">
-      <SplashText />
       <AuthForm />
     </div>
   );
